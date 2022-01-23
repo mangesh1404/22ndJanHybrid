d96909f9113604f841e16f7c713ede6160df0f79
mangesh1404 <45553084+mangesh1404@users.noreply.github.com>
2022-01-23 11:40:10 +0530

    hybrid framework part-2

--- com.HybridFramework/src/main/java/com/base/BaseClass.java
+++ com.HybridFramework/src/main/java/com/base/BaseClass.java
@@ -2,15 +2,17 @@ package com.base;
 
 import java.io.File;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.concurrent.TimeUnit;
 
 import org.apache.commons.io.FileUtils;
+import org.apache.log4j.Logger;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.remote.RemoteWebDriver;
 
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.ExtentTest;
@@ -24,9 +26,15 @@ public class BaseClass {
 	public static ExtentSparkReporter spark=null;
 	public static ExtentTest test=null;
 	
+	public static Logger log = Logger.getLogger("BaseClass");
+	
 	public void initalization() {
+		System.out.println("browser initialization started");
+		log.info("browser initialization started");
+		log.info("reading property file for the key browser");
 		String browser = PropertiesUtils.readproperty("browser");	
 		if(browser.equalsIgnoreCase("chrome")) {
+			log.info("initializing chrome browser");
 			System.setProperty("webdriver.chrome.driver", "E:/chromedriver.exe");
 			driver = new ChromeDriver();
 		}
@@ -34,22 +42,27 @@ public class BaseClass {
 			System.setProperty("webdriver.gecko.driver", "C:/geckodriver.exe");
 			driver = new FirefoxDriver();
 		}
+		log.info("maximizing browser window");
 		driver.manage().window().maximize();
+		log.info("applying browser level waits");
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 		driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
+		log.info("launching JBK offline application");
 		driver.get(PropertiesUtils.readproperty("url"));
 	}
 	
 	public void reportInit() {
+		log.info("initalizing extent report");
 		report =new ExtentReports();
 		spark = new ExtentSparkReporter(System.getProperty("user.dir")+"/target/ExtentReport.html");
 		report.attachReporter(spark);
 	}
 	
 	public String captureScreenshot(String name) {
+		log.info("capturing screenshot for failed testcase");
 		TakesScreenshot ts =(TakesScreenshot)driver;
 		File src= ts.getScreenshotAs(OutputType.FILE);
-		String path =System.getProperty("user.dir")+"/screenshots/"+name+".jpg";
+		String path =System.getProperty("user.dir")+"/screenshots/"+name+"_"+getDate()+".jpg";
 		File dest= new File(path);	
 		try {
 			FileUtils.copyFile(src, dest);
@@ -58,5 +71,14 @@ public class BaseClass {
 		}
 		return path;
 	}
+	
+	public String getDate() {
+		SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy-hh-mm-ss");
+		String date = sdf.format(new Date());
+
+		return date;
+	}
+	
+	
 	
 }
--- com.HybridFramework/src/test/java/com/tests/LoginTest.java
+++ com.HybridFramework/src/test/java/com/tests/LoginTest.java
@@ -7,14 +7,20 @@ import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Test;
 
 import com.base.BaseClass;
+import com.pages.LoginPage;
 import com.utility.PropertiesUtils;
 
 public class LoginTest extends BaseClass{
 
+	public LoginPage lp=null;
+	
+	
 	@BeforeSuite
 	public void setup(){
+		log.info("inside beforesuite");
 		initalization();
 		reportInit();
+		lp= new LoginPage(driver);
 	}
 	
 	@AfterSuite
@@ -24,13 +30,15 @@ public class LoginTest extends BaseClass{
 	
 	@Test(priority=1)
 	public void loginTest() {
-		driver.findElement(By.id("email")).sendKeys(PropertiesUtils.readproperty("username"));
-		driver.findElement(By.id("password")).sendKeys(PropertiesUtils.readproperty("password"));
-		driver.findElement(By.xpath("//button")).click();
+		log.info("executing login test");
+		lp.loginToApplication();
+		log.info("login test passed");
 	}
 	
 	@Test(priority=2)
 	public void failTest() {
+		log.info("executing fail test");
 		Assert.assertEquals(driver.getTitle(), "JavaByKiran | Log in");
+		log.info("fail test failed");
 	}
 }
